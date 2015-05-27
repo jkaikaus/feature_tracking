5764a1edf484f702316f11d693a738dd0547578f
Jamshed Kaikaus <kaikaus2@illinois.edu>
2015-05-27 15:17:04 -0500

    finalized code

--- trackingPoints.cpp
+++ trackingPoints.cpp
@@ -1,3 +1,23 @@
+/*
+ =====================================================================================
+ *
+ *       Author:  Jamshed Kaikaus, kaikaus2@illinois.edu
+ *
+ *       Description: Feature Tracking code
+ *
+ *       Version:  1.0
+ *       Created:  05/27/2015
+ *
+ =====================================================================================
+ */
+
+#include <math.h>
+#include <vector>
+#include <stdio.h>
+#include <iostream>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string>
 #include "opencv2/video/tracking.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/videoio/videoio.hpp"
@@ -7,14 +27,6 @@
 #include "opencv2/features2d/features2d.hpp"
 #include "opencv2/calib3d/calib3d_c.h"
 #include "opencv2/calib3d/calib3d.hpp"
-#include <math.h>
-#include <vector>
-#include <stdio.h>
-#include <iostream>
-#include <ctype.h>
-#include <stdlib.h>
-#include <string>
-
 
 using namespace cv;
 
